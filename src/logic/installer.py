f1b7917e42abed991cd51cae7c76d6b43104c2ec
Nathaniel Black <ncb@udel.edu>
2026-01-10 17:46:25 -0500

    Made InstallManager class

--- src/logic/installer.py
+++ src/logic/installer.py
@@ -5,15 +5,18 @@ from pathlib import Path
 import requests
 
 
-def install_assets(destination: Path):
-    URL = "https://github.com/nathancblack/CPMA-Config-Tool/releases/latest/download/assets.zip"
-    zip_path = destination / "assets.zip"
-    with requests.get(URL, stream=True) as r:
-        with open(zip_path, "wb") as f:
-            shutil.copyfileobj(r.raw, f)
-    shutil.unpack_archive(zip_path, destination)
-    os.remove(zip_path)
+class InstallManager:
+    def __init__(self, path_manager):
+        self.data = path_manager
 
+    def install_assets(self):
+        URL = "https://github.com/nathancblack/CPMA-Config-Tool/releases/latest/download/assets.zip"
+        zip_path = self.data.app_path / "assets.zip"
+        with requests.get(URL, stream=True) as r:
+            with open(zip_path, "wb") as f:
+                shutil.copyfileobj(r.raw, f)
+        shutil.unpack_archive(zip_path, self.data.app_path)
+        os.remove(zip_path)
 
-def uninstall_assets():
-    os.remove(Path())
+    def uninstall_assets(self):
+        shutil.rmtree(Path(self.data.app_path / "content"))
--- src/logic/main.py
+++ src/logic/main.py
@@ -1,25 +1,25 @@
 from pathlib import Path
 
-from installer import install_assets
+from installer import InstallManager
 from paths import PathManager
+
+root_folder_path = Path(
+    "C:/Users/natha/Programming Projects/CPMA-Config-Tool/assets/q3"
+)
+
+autoexec_cfg_path = Path(
+    "C:\\Users\\natha\\Programming Projects\\CPMA-Config-Tool\\assets\\q3\\cpma\\autoexec.cfg"
+)
 
 
 def main():
     path_manager = PathManager()
-
-    root_folder_path = Path(
-        "C:/Users/natha/Programming Projects/CPMA-Config-Tool/assets/q3"
-    )
-
-    autoexec_cfg_path = Path(
-        "C:\\Users\\natha\\Programming Projects\\CPMA-Config-Tool\\assets\\q3\\cpma\\autoexec.cfg"
-    )
+    install_manager = InstallManager(path_manager)
 
     path_manager.auto_select_paths(root_folder_path)
+    # install_manager.install_assets()
 
-    install_assets(path_manager.app_path)
-
-    # install_manager.uninstall_assets()
+    install_manager.uninstall_assets()
 
 
 main()
